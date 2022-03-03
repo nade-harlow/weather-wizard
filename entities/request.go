56b3329cd47fe60c9f2f9038fe2362876b613715
nade-harlow <omonadefranklyn@gmail.com>
2022-03-03 12:34:04 +0100

    forecast method created

--- entities/request.go
+++ entities/request.go
@@ -1,5 +1,21 @@
 package entities
 
-func (f Forcast) GetForecast()  {
+import (
+	"encoding/json"
+	"log"
+	"net/http"
+)
 
+func (f Forcast) GetForecast()  {
+	response, err := http.Get("https://api.openweathermap.org/data/2.5/weather?q=japan&appid=f2ab9d6dd296f998058bd6b4e3d0deb4")
+	if err != nil {
+		log.Println("can't reach server, ", err.Error())
+		return
+	}
+	w:= Forcast{}
+	err = json.NewDecoder(response.Body).Decode(&w)
+	if err != nil {
+		log.Println(err.Error())
+		return
+	}
 }

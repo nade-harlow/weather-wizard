bc850e404e4645f4d768f8c48fac9dabe5118c47
nade-harlow <omonadefranklyn@gmail.com>
2022-03-03 14:12:25 +0100

    forecast method initialised

--- main.go
+++ main.go
@@ -1,9 +1,18 @@
 package main
 
 import (
+	"fmt"
 	"github.com/joho/godotenv"
+	"github.com/nade-harlow/weather-wizard/entities"
 )
 
 func main() {
 	_ = godotenv.Load()
+	f := entities.Forecast{}
+	var city string
+	fmt.Print("Enter a city name: ")
+	fmt.Scan(&city)
+	f.GetForecast(city)
+	f.Print()
+
 }
